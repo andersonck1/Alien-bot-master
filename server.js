9569c8d2fd6e08ad72aa3b07c6a6562d7090aa94
andersonck1 <andersonck8@gmail.com>
2026-06-09 00:26:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ const puppeteer = require('puppeteer');
 
 async function rodarBot() {
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
     });
     const page = await browser.newPage();
     
-    // Fingir ser um navegador real
+    // Bloquear carregamento de imagens e CSS para poupar RAM
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+        if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
+            req.abort();
+        } else {
+            req.continue();
+        }
+    });
+
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36');
+    await page.goto('https://alienpg.com/?id=965744771&currency=BRL&type=2', { waitUntil: 'domcontentloaded' });
     
-    await page.goto('https://alienpg.com/?id=965744771&currency=BRL&type=2');
-    
-    console.log("Página carregada, aguardando botão...");
-    
-    // Aqui o código esperaria o botão de "Apostar" aparecer e clicaria nele
-    // await page.click('#id-do-botao-apostar'); 
-    
+    console.log("Página carregada (sem imagens/CSS).");
     await browser.close();
 }
 
